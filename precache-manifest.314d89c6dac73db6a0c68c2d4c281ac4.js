622e5a8bcfcadbf900ceb5e02fd6a5acfed192f7
Kimsanova <synak2013@gmail.com>
2020-03-05 15:45:55 +0600

    Updates

---
+++ precache-manifest.314d89c6dac73db6a0c68c2d4c281ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd555635da868c1c3461de07db5c348",
+    "url": "/task-deploy/index.html"
+  },
+  {
+    "revision": "e14507081785dac949ac",
+    "url": "/task-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77c02ff9fce77209a43a",
+    "url": "/task-deploy/static/js/2.7504aa09.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/task-deploy/static/js/2.7504aa09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14507081785dac949ac",
+    "url": "/task-deploy/static/js/main.b155303a.chunk.js"
+  },
+  {
+    "revision": "f759b2f8123c0ad6e7dc",
+    "url": "/task-deploy/static/js/runtime-main.1a7d349e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
